2a0cb9526816cef27d26b0ec52f867e0550a9c25
Brynjar Grétarsson <brynjarorng@gmail.com>
2018-12-11 11:17:52 +0000

    added menu and finished snake except for the len=2 bug and a popup when play again = N

---
+++ menu.py
@@ -0,0 +1,59 @@
+import pygame as pg
+import pygame.gfxdraw
+import snake.snake as snake
+
+pg.init()
+clock = pg.time.Clock()
+height = 800
+width = 800
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, (0,0,0))
+    return textSurface, textSurface.get_rect()    
+
+# 0 - Play game
+# 1 - Quit game
+menuState = 0
+while 1:
+    gameDisplay = pg.display.set_mode((height,width))
+    # UI elements
+    gameDisplay.fill((255,255,255))
+    largeText = pg.font.Font('freesansbold.ttf',115)
+    # menu text elements
+    PlayGameSurf, playGame = text_objects("Play Game", largeText)
+    QuitGameSurf, quitGame = text_objects("Quit", largeText)
+    playGame.center = ((height/2),(width/2) - 100)
+    quitGame.center = ((height/2),(width/2) + 100)
+    # add elements to drawing surface
+    gameDisplay.blit(PlayGameSurf, playGame)
+    gameDisplay.blit(QuitGameSurf, quitGame)
+
+    keyinput = pg.key.get_pressed()
+    if keyinput[pg.K_ESCAPE]:
+        pg.quit()
+        quit()
+    # hover play game
+    selectBar = pg.Rect(0, 0, 800, 130)
+    if menuState == 0:
+        selectBar.center = ((height/2),(width/2) - 100)
+        # if hover should be changed to selector below
+        if keyinput[pg.K_DOWN]:
+            menuState = 1
+    # hover quit
+    elif menuState == 1:
+        selectBar.center = ((height/2),(width/2) + 100)
+        # if hover should be changed to selector above
+        if keyinput[pg.K_UP]:
+            menuState = 0
+    pg.gfxdraw.box(gameDisplay, selectBar, (100,0,0,127))
+    
+    # check if to play game or quit the game
+    if keyinput[pg.K_RETURN]:
+        if menuState == 0:
+            snake.play()
+        elif menuState == 1:
+            pg.quit()
+            quit()
+
+    pg.display.update()
+    clock.tick(30)
--- snake/snake.py
+++ snake/snake.py
@@ -5,22 +5,25 @@ import math
 
 pg.init()
 
-logging.basicConfig(filename='apple_pos.log',level=logging.INFO)
+logging.basicConfig(filename='apple_pos.log', level=logging.INFO)
+
+# run variable
+run = True
 
 # global variables so that the game does not need to look for the apple
 appleX = -1
 appleY = -1
 
 # game board size
-width = 1000
-height = 1000
+width = 800
+height = 800
 SCALE = 10
 
 # init snake
 x = int(width / 2)
 y = int(height / 2)
 unitSize = 20
-snakeColor = (0,255,0)
+snakeColor = (0, 255, 0)
 snakeTail = []
 
 # base movement
@@ -29,25 +32,53 @@ moveX = 1
 moveY = 0
 
 # init apple
-appleColor = (255,0,0)
+appleColor = (255, 0, 0)
 
 # init game screen
 screen = pg.display.set_mode((width, height))
-backgroundColor = (0,0,0)
+backgroundColor = (0, 0, 0)
 background = pg.Surface(screen.get_size())
 background.fill(backgroundColor)
-
 
 screen.blit(background, (0, 0))
 pg.display.flip()
 clock = pg.time.Clock()
 
 # game logic functions
+def play():
+    # draw initial game frame and set start variables to true
+    drawGame()
+    global run
+    run = True
+    startMain = True
+    while startMain:
+        keyinput = pg.key.get_pressed()
+        if keyinput[pg.K_SPACE]:
+            resetGame()
+            startMain = start()
+
+        # bar for the text
+        largeText = pg.font.Font('freesansbold.ttf', 80)
+        selectBar = pg.Rect(0, 0, 800, 100)
+        selectBar.center = ((height/2),(width/2))
+
+        pg.gfxdraw.box(screen, selectBar, (100, 0, 0, 255))
+
+        # menu text elements
+        startSurf, startText = text_objects("press space to start", largeText)
+        startText.center = ((height/2),(width/2))
+
+        # add elements to drawing surface
+        screen.blit(startSurf, startText)
+        pg.display.flip()
+        pg.event.pump()
+        clock.tick(30)
+
 def placeApple():
     global appleX
     global appleY
-    col = math.floor(width/SCALE)
-    row = math.floor(height/SCALE)
+    col = math.floor(width / SCALE)
+    row = math.floor(height / SCALE)
     appleX = random.randint(1, col - SCALE) * SCALE
     appleY = random.randint(1, row - SCALE) * SCALE
     # Debug info
@@ -62,13 +93,15 @@ def drawGame():
 
     # apple
     pg.draw.rect(screen, appleColor, (appleY,appleX,unitSize,unitSize))
+
     # snake
     pg.draw.rect(screen, snakeColor, (x,y,unitSize,unitSize))
+
     # tail
-    
     for ind in range(len(snakeTail)):
         #print(snakeTail[ind][0], snakeTail[ind][1])
         pg.draw.rect(screen, snakeColor, (snakeTail[ind][0], snakeTail[ind][1], unitSize, unitSize))
+
     # move the snake
     if len(snakeTail) >= 1:
         snakeTail[0] = (x, y)
@@ -83,16 +116,16 @@ def drawGame():
 def detectApple():
     if appleX - unitSize/2 <= y and y <= appleX + unitSize/2 and appleY - unitSize/2 <= x and x <= appleY + unitSize/2:
         global snakeTail
-        snakeTail.append((x,y))
+        snakeTail.append((x, y))
         placeApple()
 def detectEdge():
     if x <= 0 or x >= width or y <= 0 or y >= height:
-        resetGame()
+        gameOver()
 def detectCollision():
     global x
     global y
     if (x, y) in snakeTail:
-        resetGame()
+        gameOver()
 def resetGame():
     global x
     global y
@@ -104,25 +137,64 @@ def resetGame():
     y = int(height / 2)
     moveX = 1
     moveY = 0
+def gameOver():
+    global run
+    runLoop = True
+    while runLoop:
+        keyinput = pg.key.get_pressed()
+        if keyinput[pg.K_y]:
+            resetGame()
+            return
+        elif keyinput[pg.K_n]:
+            run = False
+            runLoop = False
+            resetGame()
 
+        # bar for the text
+        largeText = pg.font.Font('freesansbold.ttf', 80)
+        selectBar = pg.Rect(0, 0, 800, 250)
+        selectBar.center = ((height/2),(width/2))
 
+        pg.gfxdraw.box(screen, selectBar, (100, 0, 0, 255))
 
-# Initial apple
-placeApple()
+        # menu text elements
+        scoreTextSurf, scoreText = text_objects("score: " + str(len(snakeTail)), largeText)
+        playAgainSurf, playAgainText = text_objects("play again? (Y/N)", largeText)
+        scoreText.center = ((height/2),(width/2) - 50)
+        playAgainText.center = ((height/2),(width/2) + 50)
 
-# game code
-while 1:
-    # limit runtime speed to 60 frames/second
-    clock.tick(30)
-    pg.event.pump()
+        # add elements to drawing surface
+        screen.blit(scoreTextSurf, scoreText)
+        screen.blit(playAgainSurf, playAgainText)
 
-    # input listener
-    keyinput = pg.key.get_pressed()
-
-    # quit the game 'ESC'
+        clock.tick(30)
+        pg.event.pump()
+        pg.display.update()
+# load the game menu for snake
+def getMenu(keyinput):
     if keyinput[pg.K_ESCAPE]:
-        raise SystemExit
-
+        global x
+        global y
+        global run
+        tmp = (x, y)
+        #x = 10
+        #y = 10
+        while 1:
+            menuState = 0
+            keyinput = pg.key.get_pressed()
+            # quit the game 'ESC'
+            clock.tick(30)
+            pg.event.pump()
+            menuState = drawMenu(keyinput)
+            if keyinput[pg.K_y]:
+                run = False
+                return
+            elif keyinput[pg.K_n]:
+                x,y = tmp
+                return
+def movePlayer(keyinput):
+    global moveX
+    global moveY
     # move the player
     if keyinput[pg.K_LEFT]:
         moveX = -moveSpeed
@@ -136,105 +208,45 @@ while 1:
     elif keyinput[pg.K_DOWN]:
         moveY = moveSpeed
         moveX = 0
+def drawMenu(keyinput):
+    # bar for the text
+    largeText = pg.font.Font('freesansbold.ttf', 80)
+    selectBar = pg.Rect(0, 0, 800, 250)
+    selectBar.center = ((height/2),(width/2))
 
-    detectApple()
-    drawGame()
-    
-        
+    pg.gfxdraw.box(screen, selectBar, (100, 0, 0, 255))
 
+    # menu text elements
+    quitTextSurf, quitText = text_objects("want to quit? (Y/N)", largeText)
+    quitText.center = ((height/2),(width/2))
 
-'''
-pg.init()
-# generate font
-pg.font.init()
-pf = pg.font.SysFont('Comic Sans MS', 30)
-
-# set screen size
-width = 640
-height = 640
-screen = pg.display.set_mode((width, height))
-pg.display.set_caption("move with arrow keys (escape key to exit)")
-
-# color (r, g, b) tuple, values 0 to 255
-white = (255, 255, 255)
-background = pg.Surface(screen.get_size())
-background.fill(white)
-
-# initial position of sprite, center of screen
-screen.blit(background, (0, 0))
-pg.display.flip()
-clock = pg.time.Clock()
-
-
-# moving box
-x = height / 2
-y = width / 2
-moveX = 6
-moveY = 0
-direction = 'x'
-boxSize = 10
-moveSpeed = 6
-
-# create the obligatory event loop
-while 1:
-    # limit runtime speed to 30 frames/second
-    clock.tick(60)
-    pg.event.pump()
-    # a key has been pressed
-    keyinput = pg.key.get_pressed()
-    # press escape key to quit game
-    if keyinput[pg.K_ESCAPE]:
-        raise SystemExit
-    # optional exit on window corner x click
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit()
-            raise SystemExit
-    
-    if keyinput[pg.K_LEFT]:
-        moveX = -moveSpeed
-        moveY = 0
-        direction = 'x'
-    elif keyinput[pg.K_RIGHT]:
-        moveX = moveSpeed
-        direction = 'x'
-        moveY = 0
-    elif keyinput[pg.K_UP]:
-        moveY = -moveSpeed
-        moveX = 0
-        direction = 'y'
-    elif keyinput[pg.K_DOWN]:
-        moveY = moveSpeed
-        moveX = 0
-        direction = 'y'
-
-    # continuous movement
-    if direction == 'x':
-        x += moveX
-    else:
-        y += moveY
-
-    # check if player has lost the game
-    # check if touching walls
-    if x >= width - boxSize or x < 0 or y > height - boxSize or y < 0:
-        break
-
-    pg.display.flip()
-    screen.blit(background, (0,0))
-    pg.draw.rect(screen, (0,255,0), (x,y,boxSize,boxSize))
+    # add elements to drawing surface
+    screen.blit(quitTextSurf, quitText)
     pg.display.update()
 
-screen.blit(background, (0,0))
-textsurface = pf.render('Game Over!', True, (0, 0, 0))
-screen.blit(textsurface,(0,0))
-while 1:
-    clock.tick(60)
-    pg.event.pump()
+# MOVE TO SEPERATE FILE
+def text_objects(text, font):
+    textSurface = font.render(text, True, (255,255,255))
+    return textSurface, textSurface.get_rect() 
 
-    keyinput = pg.key.get_pressed()
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit()
-            raise SystemExit
-    pg.display.update()
-'''
+# Initial apple
+placeApple()
+
+def start():
+    # game code
+    global run
+    while run:
+        # limit runtime speed to 30 frames/second
+        clock.tick(30)
+        pg.event.pump()
+
+        # input listener
+        keyinput = pg.key.get_pressed()
+
+        movePlayer(keyinput)        
+        detectApple()
+        getMenu(keyinput)
+        drawGame()
+
+    # exit out of game
+    return False
