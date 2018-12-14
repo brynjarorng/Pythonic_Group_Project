e868a88c3cafc6c0ee6f6c014af4cb3052173e19
Dadi Steinn <dadisteinn@gmail.com>
2018-12-14 20:39:02 +0000

    refactored ghost movement

--- whackman/sprites/Entity.py
+++ whackman/sprites/Entity.py
@@ -1,11 +1,11 @@
 import pygame as pg
 
 class Entity:
-    def __init__(self, img, char, beneath, startPos, moveDir, speed, moveCount):
+    def __init__(self, img, char, beneath, pos, moveDir, moveCount, speed):
         self.img = img
         self.char = char
         self.beneath = beneath
-        self.pos = startPos
+        self.pos = pos
         self.moveDir = moveDir
         self.speed = speed
         self.moveCount = moveCount
